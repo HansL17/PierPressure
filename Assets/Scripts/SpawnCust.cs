265b3767bb8d00a9776bfe2693d378b41dabeb08
HansL17 <hansfrancisschool@gmail.com>
2023-05-21 14:50:51 +0800

    Customer Spawn & Movement

---
+++ Assets/Scripts/SpawnCust.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCust : MonoBehaviour
+{
+    //Variables
+    public Object ObjectToSpawn; //What will spawn
+    float time;
+    float timeDelay;
+    float cusCount;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0f; //Starting time
+        timeDelay = 5f; //Delay in seconds
+        cusCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = time + 1f * Time.deltaTime;
+        if(time >= timeDelay && cusCount == 0) // If Time is equal to time delay
+        {
+            Spawn();
+            time = 0f;
+            cusCount = 0;
+        }
+
+    }
+
+    private void Spawn()
+    {
+        Instantiate(ObjectToSpawn, transform.position, transform.rotation);
+        print("Customer spawned"); //Log
+        cusCount = 1;
+    }
+}
